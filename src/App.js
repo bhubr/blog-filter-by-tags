fe102f8062301ed207c18c7409d95c5882ff1166
Benoît Hubert <benoithubert@gmail.com>
2019-04-03 14:53:00 +0200

    Lift state => centraliser state dans App

--- src/App.js
+++ src/App.js
@@ -5,11 +5,34 @@ import articles from './articles';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filter: ''
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(value) {
+    this.setState({
+      filter: value
+    });
+  }
+
   render() {
+    const tagFilter = this.state.filter;
+    let filteredArticles;
+    if (tagFilter === '') {
+      filteredArticles = articles;
+    } else {
+      filteredArticles = articles.filter(
+        article => article.tags.includes(tagFilter)
+      );
+    }
     return (
       <div className="App">
-        <FilterByTag />
-        <ArticleList articles={articles} />
+        <FilterByTag filterTag={this.handleClick} />
+        <ArticleList articles={filteredArticles} />
       </div>
     );
   }
--- src/FilterByTag.js
+++ src/FilterByTag.js
@@ -3,19 +3,6 @@ import React, { Component } from 'react';
 const tags = ['Web', 'React', 'Angular'];
 
 class FilterByTag extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      filter: ''
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(value) {
-    this.setState({
-      filter: value
-    });
-  }
   render() {
     return (
       <ul className="tags">
@@ -23,7 +10,7 @@ class FilterByTag extends Component {
         tags.map((tag, index) => (
           <li
             key={index}
-            onClick={() => this.handleClick(tag)}
+            onClick={() => this.props.filterTag(tag)}
           >
             {tag}
           </li>
